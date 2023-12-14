347296720a036dcce6af4742cb86aa4fc443a5a2
Ankur Kumar <143178279+AnkurKumar-12@users.noreply.github.com>
2023-12-14 15:26:44 +0530

    Add files via upload

---
+++ LargeNumber.java
@@ -0,0 +1,26 @@
+package FindingNumber;
+
+public class LargeNumber {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 100;
+		int c = 50;
+		if(a>b)
+		{
+			System.out.println(a+"");
+		}
+		else if (a<b && b>c)
+		{
+			System.out.println(b+"");
+		
+		}
+		else {
+			System.out.println(c+"");
+		}
+
+	}
+
+}
+
+
